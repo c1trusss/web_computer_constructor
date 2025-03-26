6df760bcc21a9ddf4696fdb5d0c48ace8743114b
c1trus <citrxs@ya.ru>
2025-03-26 06:36:07 +0300

    work

--- data/components.py
+++ data/components.py
@@ -7,21 +7,25 @@ class CPU(SqlAlchemyBase):
     __tablename__ = 'cpu'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
     brand = sqlalchemy.Column(sqlalchemy.String)
     socket = sqlalchemy.Column(sqlalchemy.String)
     cpu_group = sqlalchemy.Column(sqlalchemy.String)
-    type = sqlalchemy.Column(sqlalchemy.String)
+    delivery_type = sqlalchemy.Column(sqlalchemy.String)
     cost = sqlalchemy.Column(sqlalchemy.Integer)
     link = sqlalchemy.Column(sqlalchemy.String)
     image = sqlalchemy.Column(sqlalchemy.String)
+
+    columns = [
+        id, title, brand, socket, cpu_group, delivery_type, cost, link, image
+    ]
 
 
 class GPU(SqlAlchemyBase):
     __tablename__ = 'gpu'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
     brand = sqlalchemy.Column(sqlalchemy.String)
     memory = sqlalchemy.Column(sqlalchemy.Integer)
     developer = sqlalchemy.Column(sqlalchemy.String)
@@ -30,12 +34,16 @@ class GPU(SqlAlchemyBase):
     link = sqlalchemy.Column(sqlalchemy.String)
     image = sqlalchemy.Column(sqlalchemy.String)
 
+    columns = [
+        id, title, brand, memory, developer, memory_type, cost, link, image
+    ]
+
 
 class Core(SqlAlchemyBase):
     __tablename__ = 'core'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
     brand = sqlalchemy.Column(sqlalchemy.String)
     core_type = sqlalchemy.Column(sqlalchemy.String)
     form_factor = sqlalchemy.Column(sqlalchemy.String)
@@ -43,12 +51,16 @@ class Core(SqlAlchemyBase):
     link = sqlalchemy.Column(sqlalchemy.String)
     image = sqlalchemy.Column(sqlalchemy.String)
 
+    columns = [
+        id, title, brand, core_type, form_factor, cost, link, image
+    ]
+
 
 class CPUCoolers(SqlAlchemyBase):
     __tablename__ = 'cpu_coolers'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
     brand = sqlalchemy.Column(sqlalchemy.String)
     tdp = sqlalchemy.Column(sqlalchemy.Integer)
     socket = sqlalchemy.Column(sqlalchemy.String)
@@ -56,24 +68,32 @@ class CPUCoolers(SqlAlchemyBase):
     link = sqlalchemy.Column(sqlalchemy.String)
     image = sqlalchemy.Column(sqlalchemy.String)
 
+    columns = [
+        id, title, brand, tdp, socket, cost, link, image
+    ]
+
 
 class Disk(SqlAlchemyBase):
     __tablename__ = 'disk'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
     brand = sqlalchemy.Column(sqlalchemy.String)
     capacity = sqlalchemy.Column(sqlalchemy.Integer)
     cost = sqlalchemy.Column(sqlalchemy.Integer)
     link = sqlalchemy.Column(sqlalchemy.String)
     image = sqlalchemy.Column(sqlalchemy.String)
 
+    columns = [
+        id, title, brand, capacity, cost, link, image
+    ]
+
 
 class Motherboard(SqlAlchemyBase):
     __tablename__ = 'motherboard'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
     brand = sqlalchemy.Column(sqlalchemy.String)
     socket = sqlalchemy.Column(sqlalchemy.String)
     chipset = sqlalchemy.Column(sqlalchemy.String)
@@ -82,24 +102,32 @@ class Motherboard(SqlAlchemyBase):
     link = sqlalchemy.Column(sqlalchemy.String)
     image = sqlalchemy.Column(sqlalchemy.String)
 
+    columns = [
+        id, title, brand, socket, chipset, form_factor, cost, link, image
+    ]
+
 
 class Power(SqlAlchemyBase):
     __tablename__ = 'power'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
     brand = sqlalchemy.Column(sqlalchemy.String)
     power = sqlalchemy.Column(sqlalchemy.Integer)
     cost = sqlalchemy.Column(sqlalchemy.Integer)
     link = sqlalchemy.Column(sqlalchemy.String)
     image = sqlalchemy.Column(sqlalchemy.String)
 
+    columns = [
+        id, title, brand, power, cost, link, image
+    ]
+
 
 class RAM(SqlAlchemyBase):
     __tablename__ = 'memory'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
     brand = sqlalchemy.Column(sqlalchemy.String)
     memory = sqlalchemy.Column(sqlalchemy.Integer)
     memory_type = sqlalchemy.Column(sqlalchemy.String)
@@ -107,3 +135,7 @@ class RAM(SqlAlchemyBase):
     cost = sqlalchemy.Column(sqlalchemy.Integer)
     link = sqlalchemy.Column(sqlalchemy.String)
     image = sqlalchemy.Column(sqlalchemy.String)
+
+    columns = [
+        id, title, brand, memory, memory_type, freq, cost, link, image
+    ]
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
+import pprint
 
 from flask import Flask, render_template, request
 
+from config import COMPONENTS
 from data import db_session
+from data.components import *
 
 
 app = Flask(__name__)
@@ -12,11 +15,14 @@ app.config['SECRET_KEY'] = 'c1trus_secret_key'
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
+    db_session.global_init('db/components.db')
+    db_sess = db_session.create_session()
+
     with open("params.json", 'r', encoding='utf8') as f:
         params_data = json.load(f)
 
     params = {
-        "title": "Главная страница",
+        "page_title": "Главная страница",
         "username": "C1truS",
         "params": params_data,
         "type": "Процессор"
@@ -25,6 +31,43 @@ def index():
     if request.method == 'POST':
         selected_value = request.form.get('chooseType')
         params["type"] = selected_value
+
+    search_table = None
+    match params["type"]:
+        case 'Процессор':
+            search_table = CPU
+        case 'Видеокарта':
+            search_table = GPU
+        case 'Корпус':
+            search_table = Core
+        case 'Материнская плата':
+            search_table = Motherboard
+        case 'Оперативная память':
+            search_table = RAM
+        case 'Устройство памяти':
+            search_table = Disk
+        case 'Блок питания':
+            search_table = Power
+        case 'Кулер для ЦП':
+            search_table = CPUCoolers
+
+    details = db_sess.query(search_table).all()
+    details_list = []
+    for detail in details:
+        detail_dict = {
+            "title": detail.title,
+            "cost": detail.cost,
+            "image": f"static/img/{detail.image}",
+            "link": detail.link
+        }
+        details_list.append(detail_dict)
+
+    params['details'] = details_list
+    params["attrs"] = {}
+
+    for attr in search_table.columns:
+        variants = set(map(lambda x: getattr(x, attr.name), details))
+        params["attrs"][attr.name] = sorted(variants)
 
     return render_template('index.html', **params)
 
