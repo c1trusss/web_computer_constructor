60b7ef7d0c0f3337f6d7bc8bdf391205aa62c87c
c1trus <citrxs@ya.ru>
2025-03-29 07:28:02 +0300

    добавлена авторизация пользователей

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import json
 import os
+import datetime
 
 from flask import (
     Flask,
     render_template,
     request,
     jsonify,
-    make_response
+    make_response,
+    redirect
 )
 from flask_restful import Api
-from sqlalchemy import or_
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 import components_resourses
 from data import db_session
 from data.components import *
+from data.users import User
+from forms.user import RegisterForm, LoginForm
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c1trus_secret_key'
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(days=365)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 api = Api(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.errorhandler(404)
@@ -30,6 +44,56 @@ def not_found(_):
 @app.errorhandler(400)
 def bad_request(_):
     return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+
+@app.route('/auth')
+def authorization():
+    return render_template('authorization.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Email уже используется!")
+        user = User(
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль!",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -117,7 +181,7 @@ def main():
     api.add_resource(components_resourses.ComponentResource, '/api/<component_class>/<component_id>')
     api.add_resource(components_resourses.ComponentListResource, '/api/<component_class>')
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(port=port)
 
 
